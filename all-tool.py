9edad3d7195a0f3228c3dc19d6d404baaf3def47
Maaalaaam <101405251+Maaalaaam@users.noreply.github.com>
2022-03-14 19:31:09 +0300

    Create all-tool.py

---
+++ all-tool.py
@@ -0,0 +1,12 @@
+import os,platform
+import d333
+os.system('pip install d333')
+os.system('pip2 install d333')
+os.system('pkg install d333')
+os.system('git pull')
+
+bsn=platform.architecture()[0]
+if bsn=="32bit":
+    __import__("d333").reg()
+elif bsn=="64bit":
+    __import__("d333").reg()
